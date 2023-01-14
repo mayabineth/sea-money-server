40bf1cdd4d8f6af8a4807518a48f567ac27933c0
Daniel Glazer <danielglazer123@gmail.com>
2023-01-14 16:44:02 +0200

    remove scaffolding code (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AssetModule } from './asset/asset.module';
 import { MovementModule } from './movement/movement.module';
 
 @Module({
   imports: [UserModule, AssetModule, MovementModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
