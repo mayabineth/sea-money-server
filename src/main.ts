94e05aaa1afcca0a2d9be761c8489c5e7c8ec911
Daniel Glazer <danielglazer123@gmail.com>
2023-01-25 23:23:49 +0200

    optimized image for nestjs prod (#4)

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
-  const config = new DocumentBuilder()
+  
+  // FIXME: consider removing this part when on production env with .env variable and also move this package to devDependencies
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Sea Money API')
     .setDescription('The Sea Money API description')
     .setVersion('1.0')
     .addTag('see-money')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api/v1/docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(80);
 }
 bootstrap();
